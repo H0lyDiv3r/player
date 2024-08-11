79ffaf743c9385d2b12ce974f370bb02c60eb69f
yohaness <yohaness>
2024-08-11 21:47:15 +0300

    add playlist route handlers

--- backend/app.mjs
+++ backend/app.mjs
@@ -8,10 +8,13 @@ const app = express();
 const root = "";
 const files = "./files/";
 const directoryFile = "dir.json";
+const playlistFile = "playlists.json";
 const extensions = [".mp3", ".wav", ".ogg", ".aac", ".m4a", ".webm"];
 
 app.use(express.static("/"));
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
   const dirPath = path.join(root, req.query.dir || "");
@@ -50,6 +53,9 @@ app.get("/addDir", async (req, res) => {
   const dirPath = path.join(root, req.query.dir || "");
   const jsonFilePath = path.join(files, directoryFile);
   let data = await fs.readFile(jsonFilePath, "utf8");
+  if (!data) {
+    res.status(500).json({ message: "failed to read data" });
+  }
   let directory = JSON.parse(data);
   await scanDir(dirPath, directory);
   fs.writeFile(jsonFilePath, JSON.stringify(directory)).catch((error) => error);
@@ -59,6 +65,9 @@ app.get("/getFromDir", async (req, res) => {
   const dirPath = path.join(root, req.query.dir || "");
   const jsonFilePath = path.join(files, directoryFile);
   let data = await fs.readFile(jsonFilePath, "utf8");
+  if (!data) {
+    res.status(500).json({ message: "failed to read data" });
+  }
   //
   const chain = dirPath.split("/").slice(1).slice(0, -1);
   let directory = JSON.parse(data);
@@ -68,6 +77,110 @@ app.get("/getFromDir", async (req, res) => {
   let songs = [];
   getAllAudio(directory, songs);
   res.send(songs);
+});
+
+app.post("/createPlaylist", async (req, res) => {
+  const playlistPath = path.join(files, playlistFile);
+  // await fs.writeFile(playlistPath, JSON.stringify({}));
+  if (!(await fileType.checkFileHealth(playlistPath))) {
+    await fs
+      .writeFile(playlistPath, JSON.stringify({}))
+      .catch(() => res.status(500).json({ message: "failed to write" }));
+  }
+  let data = await fs.readFile(playlistPath, "utf8");
+  if (!data) {
+    res.status(500).json({ message: "failed to read data" });
+  }
+
+  if (Object.keys(JSON.parse(data)).includes(req.body.name)) {
+    res.status(500).json({ message: "already exists" });
+  }
+  let playlists = JSON.parse(data);
+  playlists[req.body.name] = [];
+  await fs.writeFile(playlistPath, JSON.stringify(playlists));
+  res.send();
+});
+
+app.delete("/deletePlaylist", async (req, res) => {
+  const playlistPath = path.join(files, playlistFile);
+  // await fs.writeFile(playlistPath, JSON.stringify({}));
+  if (!(await fileType.checkFileHealth(playlistPath))) {
+    res.status(400).json({ message: "you dont have any playlists" });
+  }
+  let data = await fs.readFile(playlistPath, "utf8");
+  if (!data) {
+    res.status(500).json({ message: "failed to read data" });
+  }
+
+  if (!Object.keys(JSON.parse(data)).includes(req.body.name)) {
+    res.status(400).json({ message: "playlist doenst exist" });
+  }
+  let playlists = JSON.parse(data);
+  delete playlists[req.body.name];
+  await fs.writeFile(playlistPath, JSON.stringify(playlists));
+  res.send();
+});
+
+app.get("/getPlaylist", async (req, res) => {
+  const playlistPath = path.join(files, playlistFile);
+  // await fs.writeFile(playlistPath, JSON.stringify({}));
+  if (!(await fileType.checkFileHealth(playlistPath))) {
+    res.status(400).json({ message: "you dont have any playlists" });
+  }
+  let data = await fs.readFile(playlistPath, "utf8");
+  if (!data) {
+    res.status(500).json({ message: "failed to read data" });
+  }
+  let playlist = JSON.parse(data)[req.body.name];
+  res.send(playlist);
+});
+app.post("/addToPlaylist", async (req, res) => {
+  const playlistPath = path.join(files, playlistFile);
+  // await fs.writeFile(playlistPath, JSON.stringify({}));
+  if (!(await fileType.checkFileHealth(playlistPath))) {
+    res.status(500).json({ message: "cant write" });
+    // await fs
+    //   .writeFile(playlistPath, JSON.stringify([]))
+    //   .catch(() => res.status(500).json({ message: "failed to write" }));
+  }
+  let data = await fs.readFile(playlistPath, "utf8");
+  if (!data) {
+    res.status(500).json({ message: "failed to read data" });
+  }
+  let playlists = JSON.parse(data);
+  if (!playlists[req.body.playlist]) {
+    res.status(500).json({ message: "Playlist doenst exist" });
+  }
+  let target = playlists[req.body.playlist];
+  target.push({
+    id: new Date().valueOf(),
+    name: req.body.name,
+    path: req.body.path,
+    type: req.body.type,
+  });
+  await fs.writeFile(playlistPath, JSON.stringify(playlists));
+  res.send();
+});
+
+app.delete("/deleteFromPlaylist", async (req, res) => {
+  const playlistPath = path.join(files, playlistFile);
+  // await fs.writeFile(playlistPath, JSON.stringify({}));
+  if (!(await fileType.checkFileHealth(playlistPath))) {
+    res.status(500).json({ message: "cant write" });
+    // await fs
+    //   .writeFile(playlistPath, JSON.stringify([]))
+    //   .catch(() => res.status(500).json({ message: "failed to write" }));
+  }
+  let data = await fs.readFile(playlistPath, "utf8");
+  if (!data) {
+    res.status(500).json({ message: "failed to read data" });
+  }
+  let playlists = JSON.parse(data);
+  playlists[req.body.name] = playlists[req.body.name].filter(
+    (item) => item.id != req.body.id,
+  );
+  await fs.writeFile(playlistPath, JSON.stringify(playlists));
+  res.send();
 });
 
 ///
@@ -117,7 +230,6 @@ const getAllAudio = async (dir, store) => {
         path: path.join(dir[item], item),
         type: "file",
       });
-      console.log(songs);
     } else {
       getAllAudio(dir[item], songs);
     }
@@ -143,6 +255,12 @@ const fileType = {
       .then(() => true)
       .catch(() => false);
   },
+  checkFileHealth: async (dir) => {
+    return fs
+      .access(dir, fs.constants.F_OK)
+      .then(() => true)
+      .catch(() => false);
+  },
   containsMusicFileOrDir: async (dir) => {
     if (await fileType.isDir(dir)) {
       const files = await fs.readdir(dir, { withFileTypes: true });
@@ -163,13 +281,3 @@ const fileType = {
 app.listen(3000, () => {
   console.log("listening on port 3000");
 });
-
-// const dir = "/";
-// const files = fs.readdirSync(dir);
-
-// for (const file of files) {
-//   console.log(fs.statSync(dir + file).isDirectory());
-//   if (fs.statSync(dir + file).isFile()) {
-//     console.log(path.resolve(dir + file));
-//   }
-// }
