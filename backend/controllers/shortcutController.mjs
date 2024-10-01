df9ce30106a21fd3e23540d90eab55162104b737
yohaness <yohaness>
2024-10-01 10:41:26 +0300

    error handlers

--- backend/controllers/shortcutController.mjs
+++ backend/controllers/shortcutController.mjs
@@ -5,59 +5,71 @@ import { createFile, fileType } from "../utils/file.mjs";
 import * as fs from "fs/promises";
 
 export const shortcutController = {
-  getShortcuts: async (req, res) => {
-    const shortcutPath = path.join(files, shortcutsFile);
-    if (!(await fileType.checkFileHealth(shortcutPath))) {
-      await createFile("shortcuts", JSON.stringify({ shortcuts: [] }));
+  getShortcuts: async (req, res, next) => {
+    try {
+      const shortcutPath = path.join(files, shortcutsFile);
+      if (!(await fileType.checkFileHealth(shortcutPath))) {
+        await createFile("shortcuts", JSON.stringify({ shortcuts: [] }));
+      }
+      const data = await fs.readFile(shortcutPath);
+      if (!data) {
+        return res.status(500).json({ message: "failed to read data" });
+      }
+      let shortcuts = JSON.parse(data)["shortcuts"];
+      return res.status(200).send(shortcuts);
+    } catch (error) {
+      next(error);
     }
-    const data = await fs.readFile(shortcutPath);
-    if (!data) {
-      return res.status(500).json({ message: "failed to read data" });
-    }
-    let shortcuts = JSON.parse(data)["shortcuts"];
-    res.send(shortcuts);
   },
-  addShortcut: async (req, res) => {
-    const shortcutPath = path.join(files, shortcutsFile);
-    if (!(await fileType.checkFileHealth(shortcutPath))) {
-      console.log("file is unreadable");
-      await createFile("shortcuts", JSON.stringify({ shortcuts: [] }));
-    }
-    const data = await fs.readFile(shortcutPath);
-    if (!data) {
-      return res.status(500).json({ message: "failed to read data" });
-    }
-    let shortcuts = JSON.parse(data)["shortcuts"];
-    if (shortcuts.some((item) => item.name === req.body.name)) {
-      return res.status(403).json({ message: "shortcut already exists" });
-    }
-    shortcuts.push({
-      name: req.body.name,
-      path: req.body.path,
-      active: req.body.active,
-    });
+  addShortcut: async (req, res, next) => {
+    try {
+      const shortcutPath = path.join(files, shortcutsFile);
+      if (!(await fileType.checkFileHealth(shortcutPath))) {
+        await createFile("shortcuts", JSON.stringify({ shortcuts: [] }));
+      }
+      const data = await fs.readFile(shortcutPath);
+      if (!data) {
+        return res.status(500).json({ message: "failed to read data" });
+      }
+      let shortcuts = JSON.parse(data)["shortcuts"];
+      if (shortcuts.some((item) => item.name === req.body.name)) {
+        return res.status(409).json({ message: "shortcut already exists" });
+      }
+      shortcuts.push({
+        name: req.body.name,
+        path: req.body.path,
+        active: req.body.active,
+      });
 
-    console.log(shortcuts);
-    await createFile("shortcuts", JSON.stringify({ shortcuts }));
-    res.send(shortcuts);
+      await createFile("shortcuts", JSON.stringify({ shortcuts }));
+      return res.status(200).send(shortcuts);
+    } catch (error) {
+      next(error);
+    }
   },
-  deleteShortcut: async (req, res) => {
-    const shortcutPath = path.join(files, shortcutsFile);
-    if (!(await fileType.checkFileHealth(shortcutPath))) {
-      await createFile("shortcuts", JSON.stringify({ shortcuts: [] }));
-    }
-    const data = await fs.readFile(shortcutPath);
-    if (!data) {
-      return res.status(500).json({ message: "failed to read data" });
-    }
-    let shortcuts = JSON.parse(data)["shortcuts"];
-    let newShortcuts = shortcuts.filter((item) => item.name != req.query.name);
+  deleteShortcut: async (req, res, next) => {
+    try {
+      const shortcutPath = path.join(files, shortcutsFile);
+      if (!(await fileType.checkFileHealth(shortcutPath))) {
+        await createFile("shortcuts", JSON.stringify({ shortcuts: [] }));
+      }
+      const data = await fs.readFile(shortcutPath);
+      if (!data) {
+        return res.status(500).json({ message: "failed to read data" });
+      }
+      let shortcuts = JSON.parse(data)["shortcuts"];
+      let newShortcuts = shortcuts.filter(
+        (item) => item.name != req.query.name,
+      );
 
-    await createFile("shortcuts", JSON.stringify({ shortcuts: newShortcuts }));
-    // await fs.writeFile(
-    //   shortcutPath,
-    //   JSON.stringify({ shortcuts: newShortcuts }),
-    // );
-    res.send(newShortcuts);
+      await createFile(
+        "shortcuts",
+        JSON.stringify({ shortcuts: newShortcuts }),
+      );
+
+      return res.status(200).send(newShortcuts);
+    } catch (error) {
+      next(error);
+    }
   },
 };
